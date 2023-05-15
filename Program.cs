9b09b810558f8697ad691201dbf57f6fa4cf8a37
Булатецкая <116424178+bulatezkay@users.noreply.github.com>
2023-05-15 15:16:32 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kv_k
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c = 0;
+            a = float.Parse(Console.ReadLine()); 
+            b = float.Parse(Console.ReadLine()); 
+            c = a / b;
+            Console.WriteLine(c); 
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
